d55a48e1850e6b25b66bf169ea1ce81d7c0d8608
gnikoloff <nikoloffgeorgi@gmail.com>
2018-01-19 00:00:19 +0100

    made renderLoop classs

---
+++ georgi-gl/core/render-loop.js
@@ -0,0 +1,24 @@
+export default class RenderLoop {
+    constructor (fps) {
+        this.oldTime = 0
+        this.updateAnimationFrame = this.updateAnimationFrame.bind(this)
+
+        this.cb = null
+    }
+    start (cb) {
+        this.cb = cb
+        this.updateAnimationFrame()
+    }
+    stop (cb) {
+        window.cancelAnimationFrame(this.rAf)
+        if (cb) cb()
+    }
+    updateAnimationFrame () {
+        this.rAf = window.requestAnimationFrame(this.updateAnimationFrame)
+        const currentTime = performance.now() / 1000
+        const deltaTime = currentTime - this.oldTime    
+        this.oldTime = currentTime
+        
+        this.cb(deltaTime)
+    }
+}
